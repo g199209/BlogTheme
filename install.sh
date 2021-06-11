de56e794a2be61393017c4c23cb2cc829f6943f9
Gao Mingfei <g199209@gmail.com>
2021-06-11 16:19:32 +0800

    [Enhance] Encrypt secret config.
    Signed-off-by: Gao Mingfei <g199209@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+enc_key="1234567890"
+
 echo "Copying configuration files..."
 rm ../../_config.yml
 ln ./_config_root.yml ../../_config.yml
@@ -11,6 +13,9 @@ mkdir ../../source/tags
 ln ./tag_index.md ../../source/tags/index.md
 ln ./wechat.md ../../source/wechat.md
 echo "gaomf.cn" >> ../../source/CNAME
+
+echo "Decrypting enc config..."
+openssl aes256 -base64 -k ${enc_key} -d -in _config_enc -out ../../_config_enc.yml
 
 echo "Copying markdown templates..."
 rm ../../scaffolds/*
